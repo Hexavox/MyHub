0bc1982adbd53829c30770390274f5cb2bb26cf8
Hexavox <hexavox01@gmail.com>
2026-09-08 10:22:09 -0700

    made context menu bigger

--- Main.lua
+++ Main.lua
@@ -325,7 +325,7 @@ end)
 
 local contextMenu = Instance.new("Frame")
 contextMenu.Name = "PathContextMenu"
-contextMenu.Size = UDim2.fromOffset(200, 126)
+contextMenu.Size = UDim2.fromOffset(200, 150)
 contextMenu.BackgroundColor3 = Color3.fromRGB(12, 14, 20)
 contextMenu.BackgroundTransparency = 0.15
 contextMenu.BorderSizePixel = 0
